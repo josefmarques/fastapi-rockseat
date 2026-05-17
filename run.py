b028f808453a37767ff1497813dc7771eb312b95
Jose Marques <josemarques.moc@gmail.com>
2026-05-17 16:55:12 -0300

    feat: Implementing main dir with first route

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
+import uvicorn
 
-print("Olá mundo")
+if __name__=="__main__":
+    uvicorn.run(
+        "src.main.server.server:app",
+        host='0.0.0.0',
+        port=3001,
+        reload=True
+    )
---
+++ src/main/routes/users_routes.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+
+users_routes = APIRouter(tags=["Usuários"])
+
+@users_routes.post("/users")
+async def create_user():
+    return JSONResponse(
+        content={"Olá": "Mundo"},
+        status_code=200
+    )
+    
