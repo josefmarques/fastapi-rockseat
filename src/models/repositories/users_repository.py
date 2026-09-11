69907d81a0bb502c90309de3a4e65188fd8934c3
Jose Marques <josemarques.moc@gmail.com>
2026-09-11 07:55:28 -0300

    Feat: implementing user_repository

--- src/models/repositories/users_repository.py
+++ src/models/repositories/users_repository.py
@@ -1,4 +1,5 @@
-from sqlalchemy import insert, select
+# pylint: disable=w0212
+from sqlalchemy import insert, select, update, delete
 from src.models.entities.users import Users
 from src.models.settings.database_connection_handler import DBConnectionHandler
 
@@ -16,10 +17,28 @@ class UsersRepository:
                 select(Users)
                 .where(Users.c.user_name == user_name)
             )
-            result = await query.all() #Executa a consulta e aguarda o resultado. O método all() retorna todos os registros que atendem à condição especificada.
-            return result #Retorna a lista de usuários encontrados para o chamador da função.
+            result = await db.session.execute(query) #Executa a consulta SQL montada, buscando todos os registros na tabela Users onde o campo user_name corresponde ao valor fornecido. O await indica que a função deve aguardar a conclusão da operação antes de prosseguir.
+            rows = result.fetchall() #Recupera todas as linhas retornadas pela consulta. Cada linha é representada como um objeto Row, que permite acessar os valores das colunas por nome.
+
+            users_list = [dict(row._mapping) for row in rows]
+            return users_list #Retorna a lista de dicionários, onde cada dicionário representa um usuário encontrado na consulta. Cada chave do dicionário corresponde ao nome da coluna na tabela Users, e o valor associado é o dado armazenado naquela coluna para o usuário específico.
+
+    async def update_user_by_name(self, user_name:str, update_infos: dict) -> None: # Define o método como assíncrono, recebendo o nome de quem será atualizado e um dicionário com os novos dados.
+        async with DBConnectionHandler() as db: # Abre uma sessão segura com o banco de dados.
+            query = (
+                update(Users) # Inicia a construção de uma instrução SQL de atualização na tabela Users.
+                .where(Users.c.user_name == user_name) #
+                .values(**update_infos) # Desempacota o dicionário update_infos, aplicando os novos valores nas colunas correspondentes.
+            )
+            await db.session.execute(query) # Executa a instrução SQL de atualização.
+            await db.session.commit() # Confirma a transação, garantindo que as alterações sejam salvas no banco de dados.
 
 
-
-
-
+    async def delete_users_by_name(self, user_name: str) -> None: # Define o método como assíncrono, recebendo o nome do usuário que será deletado.
+        async with DBConnectionHandler() as db: # Abre a sessão segura com o banco de dados.
+            query = (
+                delete(Users) # Inicializa a construção da instrução SQL de DELETE na tabela Users.
+                .where(Users.c.user_name == user_name) # Define a condição (WHERE) estrita para deletar apenas o registro correspondente.
+            )
+            await db.session.execute(query) # Dispara o comando de deleção para o banco.
+            await db.session.commit() # Efetiva a exclusão no banco de dados gravando a transação.
--- src/models/repositories/users_repository_test.py
+++ src/models/repositories/users_repository_test.py
@@ -2,13 +2,39 @@ import pytest
 from .users_repository import UsersRepository
 
 @pytest.mark.asyncio
-@pytest.mark.skip(reason="Este teste é apenas um exemplo e não deve ser executado em produção.")
+@pytest.mark.skip(reason="Insert in DB.")
 async def test_insert_users():
     new_user = {
-        "user_name": "NomeDeTeste",
-        "age": 99,
-        "uf": "SP"
+        "user_name": "Ana Marques",
+        "age": 69,
+        "uf": "BA"
     }
 
     repo = UsersRepository()
     await repo.insert_users(new_user)
+
+@pytest.mark.asyncio
+@pytest.mark.skip(reason="Select in DB.")
+async def test_get_users_by_name():
+    repo = UsersRepository()
+    response = await repo.get_users_by_name("NomeDeTeste")
+    print(response)  # Adicione esta linha para depuração
+
+@pytest.mark.asyncio
+#@pytest.mark.skip(reason="Update in DB.")
+async def test_update_users():
+    update_data = {
+        "user_name": "Murilo César",
+        "age": 79,
+        "uf": "MG"  
+    }
+    repo = UsersRepository()
+    # Chama a função passando o nome do usuário e o dicionário com os dados novos
+    await repo.update_user_by_name("Murilo César", update_data)
+
+@pytest.mark.asyncio
+@pytest.mark.skip(reason="Delete in DB.")
+async def test_delete_users():
+    repo = UsersRepository()
+    # Chama a função passando apenas o nome do usuário a ser deletado
+    await repo.delete_users_by_name("NomeDeTeste")
